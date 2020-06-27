3846e71735abcc91a5968cff93d04af7225e841e
ammon-turner <44066069+ammon-turner@users.noreply.github.com>
2020-06-26 20:21:10 -0600

    Finished Codecademy OOP
    Finished the Object Oriented Programming section on Codecademy for Ruby.

--- Ruby/odin_project_notes/oop.rb
+++ Ruby/odin_project_notes/oop.rb
@@ -138,3 +138,152 @@ end
 my_computer = Computer.new("Bill", "BuyGold")
 
 ## Code Academy Object Oriented Programming Part II
+
+# Ruby has both public an private methods for classes. They are declared using the
+# keywords public and private. 
+
+# We can access variables in a class using attr_reader and attr_writer. If we pass
+# the names of our variables as symbols to attr_reader and attr_writer we can read
+# or modify them without writing our own methods.
+
+# if we have a variable that we want to both read and write, we don't need to add
+# attr_reader and attr_writer we can use attr_accessor to do both.
+
+# EXAMPLE
+class Person
+  attr_reader :name
+  attr_accessor :job
+  
+  def initialize(name, job)
+    @name = name
+    @job = job
+  end
+end
+
+# Modules can store things useful bits of code, but unlike classes they can't have
+# subclasses and can't create instances. It's just a way to get things used frequently
+# out of the way in a different file.
+
+# Modules are named with CamelCase, just like classes. It goes against best practice to
+# include a variable in a module because you don't change things in modules.
+# But it is a very good idea to store constants in modules since they don't change.
+
+# Remember constants in Ruby are written in ALL_CAPS, Ruby will allow you to change a 
+# constant after it has been initialized, but it will warn you that you are trying
+# to change a constant.
+
+module MyLibrary
+  FAVE_BOOK = "Is It Wrong to Try to Pick Up Girls in a Dungeon?"
+end
+
+# Ruby has a scope resolution operator, a way to tell where a specific variable is located
+# the Ruby Math module has a constant called PI which can be accessed with Math::PI
+
+# Some modules are included in the interpreter by default like Math, but others need to
+# be included using require 'module' Just like include in Python
+
+# there is also the keyword include so it's exactly like Python in that way.
+
+class Angle
+  include Math
+  attr_accessor :radians
+  
+  def initialize(radians)
+    @radians = radians
+  end
+  
+  def cosine
+    cos(@radians)
+  end
+end
+
+acute = Angle.new(1)
+acute.cosine
+
+# using modules inside classes is called a mixin. You can write a method
+# in a module and then include it in a class and access the method when
+# you call an instance of that class with the method.
+
+# EXAMPLE
+module Action
+  def jump
+    @distance = rand(4) + 2
+    puts "I jumped forward #{@distance} feet!"
+  end
+end
+
+class Rabbit
+  include Action
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Cricket
+  include Action
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
+
+peter = Rabbit.new("Peter")
+jiminy = Cricket.new("Jiminy")
+
+peter.jump
+jiminy.jump
+
+# include allows an instance of a class to use the module's methods
+# but if you use extends the class itself can use the methods
+
+# EXAMPLE
+# ThePresent has a .now method that we'll extend to TheHereAnd
+
+module ThePresent
+  def now
+    puts "It's #{Time.new.hour > 12 ? Time.new.hour - 12 : Time.new.hour}:#{Time.new.min} #{Time.new.hour > 12 ? 'PM' : 'AM'} (GMT)."
+  end
+end
+
+class TheHereAnd
+  extend ThePresent
+end
+
+TheHereAnd.now
+
+# Codecademy Banking on Ruby Assignment:
+
+class Account
+  attr_reader :name, :balance
+  
+  def initialize(name, balance=100)
+    @name = name
+    @balance = balance
+  end
+
+  public
+  def display_balance(pin_number)
+    pin_number == pin  ? (puts "Balance: $#{@balance}") : (puts pin_error)
+  end
+
+  def withdraw(pin_number, amount)
+    if pin_number == pin
+      @balance -= amount
+      puts "Withdrew #{amount}. New balance: $#{@balance}."
+    else
+      puts pin_error
+    end
+  end
+
+  private
+  def pin
+    @pin = 1234
+  end
+
+  def pin_error
+    return "Access denied: incorrect PIN."
+  end
+end
+
+checking_account = Account.new("Bezos", 163_300_000_000)
