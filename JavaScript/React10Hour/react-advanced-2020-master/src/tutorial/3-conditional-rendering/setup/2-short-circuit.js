abfc3fb754a96ee95175d67a773234e6eaa62ca6
ammon-turner <44066069+ammon-turner@users.noreply.github.com>
2020-11-25 13:11:19 -0700

    Continued the 10 Hour course
    Just some continued edits from the React 10 Hour course

--- JavaScript/React10Hour/react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ JavaScript/React10Hour/react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,34 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState('')
+  const [isError, setIsError] = useState(false)
+  const firstValue = text || 'first';
+  const secondValue = text && 'second';
+  console.log(firstValue)
+  console.log(secondValue)
 
-  return <h2>short circuit</h2>;
+  // The reason this is being covered is because we can't use if statements
+  // in the middle of a jsx return with React, but there are times we will
+  // want to display things conditionally like that. We can use short-circuit
+  // evaluation to get the same sort of result as an if statement but still
+  // have it return something to make jsx happy
+
+  return (<>
+  {/*<h1>{firstValue}</h1>
+  <h1>value : {secondValue}</h1>*/}
+
+  <h1>{text || 'Kirito'}</h1>
+  <button className="btn" onClick={() => setIsError(!isError)}>toggle error</button>
+  {text && <h2>SAO</h2>}
+  {!text && <h2>sword art online</h2>}
+  <br />
+  {isError && <>
+  <h1>Error...</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  </>}
+  {isError ? <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> : <p>The cake is a lie.</p>}
+  </>);
 };
 
 export default ShortCircuit;
