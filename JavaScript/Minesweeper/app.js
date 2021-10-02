88263f3e1bd7af8bd75a1cfed0163af081bee9b3
Ammon <ammon42@gmail.com>
2021-10-01 16:25:22 -1000

    Need to rethink some things
    I'm probably going to need to switch these to objects not a 3d array, because this is getting to be problematic.

--- JavaScript/Minesweeper/app.js
+++ JavaScript/Minesweeper/app.js
@@ -10,15 +10,79 @@ document.addEventListener('DOMContentLoaded', () => {
     mines[i] = new Array(fullSize);
   }
 
-  // create the grid of mines
-  for (var i = 0; i < mines.length; i++) {
+  for (i = 0; i < fullSize; i++) {
+    for (j = 0; j < fullSize; j++) {
+      mines[i][j] = { 'mined': false, 'nearCount': 0 };
+    }
+  }
+
+  // place the mines
+  for (var i = 0; i < mineCount; i++) {
     let x = Math.trunc(Math.random() * mineCount);
     let y = Math.trunc(Math.random() * mineCount);
 
-    mines[x][y] = true;
+    mines[x][y]['mined'] = true;
+  }
+
+  // Calculate the numbers in surrounding squares
+  for (var i = 0; i < fullSize; i++) {
+    for (var j = 0; j < fullSize; i++) {
+      if (mines[i][j]['mined']) {
+        if (i > 0 && j > 0 && i != fullSize - 1 && j != fullSize - 1) {
+          mines[i-1][j-1]['nearCount'] += 1;
+          mines[i-1][j]['nearCount'] += 1;
+          mines[i-1][j+1]['nearCount'] += 1;
+          mines[i][j-1]['nearCount'] += 1;
+          mines[i][j+1]['nearCount'] += 1;
+          mines[i+1][j-1]['nearCount'] += 1;
+          mines[i+1][j]['nearCount'] += 1;
+          mines[i+1][j+1]['nearCount'] += 1;
+        } else if(i === 0 && j > 0 && j != fullSize - 1) {
+          mines[i][j-1]['nearCount'] += 1;
+          mines[i][j+1]['nearCount'] += 1;
+          mines[i+1][j-1]['nearCount'] += 1;
+          mines[i+1][j]['nearCount'] += 1;
+          mines[i+1][j+1]['nearCount'] += 1;
+        } else if (j === 0 && i > 0 && i != fullSize - 1) {
+          mines[i+1][j]['nearCount'] += 1;
+          mines[i+1][j+1]['nearCount'] += 1;
+          mines[i][j+1]['nearCount'] += 1;
+          mines[i-1][j]['nearCount'] += 1;
+          mines[i-1][j+1]['nearCount'] += 1;
+        } else if (i === fullSize - 1 && j > 0 && j != fullSize - 1) {
+          mines[i-1][j-1]['nearCount'] += 1;
+          mines[i-1][j]['nearCount'] += 1;
+          mines[i-1][j+1]['nearCount'] += 1;
+          mines[i][j-1]['nearCount'] += 1;
+          mines[i][j+1]['nearCount'] += 1;
+        } else if (j === fullSize - 1 && i > 0 && i != fullSize - 1) {
+          mines[i-1][j-1]['nearCount'] += 1;
+          mines[i-1][j]['nearCount'] += 1;
+          mines[i][j-1]['nearCount'] += 1;
+          mines[i+1][j-1]['nearCount'] += 1;
+          mines[i+1][j]['nearCount'] += 1;
+        } else if (i === 0 && j === 0) {
+          mines[i+1][j]['nearCount'] += 1;
+          mines[i][j+1]['nearCount'] += 1;
+          mines[i+1][j+1]['nearCount'] += 1;
+        } else if (i === fullSize - 1 && j === 0) {
+          mines[i-1][j]['nearCount'] += 1;
+          mines[i][j+1]['nearCount'] += 1;
+          mines[i-1][j+1]['nearCount'] += 1;
+        } else if (i === 0 && j === fullSize - 1) {
+          mines[i+1][j]['nearCount'] += 1;
+          mines[i][j-1]['nearCount'] += 1;
+          mines[i+1][j-1]['nearCount'] += 1;
+        } else if (i === fullSize - 1 && j === fullSize - 1) {
+          mines[i-1][j]['nearCount'] += 1;
+          mines[i-1][j-1]['nearCount'] += 1;
+          mines[i][j-1]['nearCount'] += 1;
+        }
+      }
+    }
   }
   
-  // create a grid of squares
+  // display the grid of squares
   for (var i = 0; i < fullSize; i++) {
     let row = document.createElement('div');
     row.classList.add('row');
@@ -26,8 +90,10 @@ document.addEventListener('DOMContentLoaded', () => {
     for (var j = 0; j < fullSize; j++) {
       let square = document.createElement('div');
       square.classList.add('square');
-      if (mines[i][j]) {
+      if (mines[i][j]['mined']) {
         square.classList.add('mine');
+      } else {
+        square.innerHTML = mines[i][j]['nearCount'];
       }
       row.appendChild(square);
     }
