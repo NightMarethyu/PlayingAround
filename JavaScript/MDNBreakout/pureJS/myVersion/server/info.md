942d48f73ca5cb53bf72e4a7d77959c45d42affc
Ammon <ammon42@gmail.com>
2021-10-14 17:06:00 -1000

    Tried a Golang server
    I tried to serve my files with a Golang server and I decided to not continue that path

---
+++ JavaScript/MDNBreakout/pureJS/myVersion/server/info.md
@@ -0,0 +1,3 @@
+Don't use these files
+
+I wanted to try serving my website with a simple Golang server, but it doesn't fix the import issues
---
+++ JavaScript/MDNBreakout/pureJS/myVersion/server/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", http.StripPrefix("/", fileServer))
+
+	http.ListenAndServe(":5050", nil)
+}
