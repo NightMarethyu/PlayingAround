dec8242ebe5b7773cb6099cc2e09ee9f9d6dccdd
Ammon <ammon42@gmail.com>
2022-01-12 15:12:39 -1000

    Added comments
    I've added comments to ensure more points for the assignment

--- CIS205/labs/lab01.01.py
+++ CIS205/labs/lab01.01.py
@@ -1,5 +1,8 @@
 from sys import argv, exit
 
+# This method will do some basic error checking, make sure the user
+# added a command line argument and make sure the file exists
+# it will then send the file to the checkStatement function
 def main():
     if (len(argv) > 1):
         try:
@@ -12,25 +15,32 @@ def main():
         print("ERROR: Please provide file as command line argument")
         exit(1)
 
+# This function has a few different if statements each one will check for a different
+# aspect that will make a line not a statement
 def checkStatement(file):
     ns = " NOT A STATEMENT"
     pronouns = ["he", "she", "it", "they", "his", "her", "him", "them", "hers", "theirs", "their", "herself", "himself", "themselves", "itself", ""]
-    interogatives = ["who", "what", "when", "where", "why", "how"]
+    interrogatives = ["who", "what", "when", "where", "why", "how"]
     for line in file:
         line = line.strip()
         lineArr = line.lower().split(" ")
+        # check if the last character is a question mark
         if (line[-1] == "?"):
             print(line + ns)
             continue
+        # Check if the line is only one word
         if (len(lineArr) == 1):
             print(line + ns)
             continue
+        # check if the line contains pronouns
         if any(x in lineArr for x in pronouns):
             print(line + ns)
             continue
-        if any(x in interogatives for x in lineArr):
+        # check if the line contains interrogatives
+        if any(x in interrogatives for x in lineArr):
             print(line + ns)
             continue
+        # We assume that if the line doesn't fail any of the tests it is a statement
         print(line + " STATEMENT")
 
 if __name__ == "__main__":
