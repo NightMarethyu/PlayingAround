d2195f6b2d2889bca96ab8a81a23cf391570afb3
Ammon <ammon42@gmail.com>
2022-04-12 07:51:10 -1000

    End of CIS205
    I have finished the CIS 205 class. this is the last update for that class

--- CIS205/labs/lab06.02.py
+++ CIS205/labs/lab06.02.py
@@ -1,6 +1,12 @@
 from sys import argv
 
+# This fuction loads the file given in argv[1] and passes it to
+# the evaluation function. This will also print the values that
+# are calculated from the evalString function.
 def main():
+    first = '6 6 * 2 7 * - 2 /'
+    print('Result of "' + first + '": ', end='')
+    print(evalString(first), end="\n\n")
     if(len(argv) == 2):
         try:
             with open(argv[1]) as f:
@@ -17,6 +23,9 @@ def main():
         print("Must include filename as commandline parameter")
         exit(-1)
 
+# This function will evaluate the string as a Reverse Polish Notation
+# mathmatical equation. It will split the sting on each space " "
+# and will return the computed value as a float
 def evalString(rpn):
     vals = rpn.strip().split()
     stack = []
@@ -31,6 +40,10 @@ def evalString(rpn):
             stack.append(eqate)
     return stack[0]
 
+# Due to python's built-in string methods not having a great
+# float checking tool, I built this one using a try except
+# block, it will try to convert the provided variable to a float
+# if it succeeds True will be returned
 def isfloat(x):
     try:
         float(x)
@@ -38,6 +51,9 @@ def isfloat(x):
     except:
         return False
 
+# This will evaluate the given values using the provided
+# string which should be one of "+-/*" otherwise it will
+# return None, since the provided equation isn't there.
 def evaluateVals(x, y, f):
     if (f == "+"):
         return x + y
