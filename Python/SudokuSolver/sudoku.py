37296e5fda05548c106fc3aa4696108a8298e56d
Ammon <ammon42@gmail.com>
2022-04-12 19:19:01 -1000

    Parsing boxes rows and columns
    I've added some functions to parse the boxes, rows, and columns. I've determined that I need another class for the individual squares. I might actually need classes for each item with a square being nested in each.

--- Python/SudokuSolver/sudoku.py
+++ Python/SudokuSolver/sudoku.py
@@ -1,20 +1,67 @@
-from hashlib import new
-
-
 class Sudoku:
     def __init__(self, puzzle):
         self.puzzle = puzzle
         self.rows = []
-        self.columns = [[]] * 9
+        self.columns = []
         self.boxes = []
-        self.parseRowsColumns()
+        self.parseRows()
+        self.parseColumns()
+        self.parseBoxes()
+        # print("## Rows ##")
         # for r in self.rows:
         #     print(r)
+        # print("## Columns ##")
         # for c in self.columns:
         #     print(c)
+        print('## Boxes ##')
+        for b in self.boxes:
+            print(b)
         
-    def parseRowsColumns(self):
+    def parseRows(self):
         for i in self.puzzle:
             self.rows.append(i)
-            for count, val in enumerate(i):
-                self.columns[count].append(val)
+    
+    def parseColumns(self):
+        temp = []
+        for i in range(9):
+            for j in range(9):
+                temp.append(self.rows[j][i])
+            self.columns.append(temp)
+            temp = []
+    
+    def parseBoxes(self):
+        box1 = []
+        box2 = []
+        box3 = []
+        box4 = []
+        box5 = []
+        box6 = []
+        box7 = []
+        box8 = []
+        box9 = []
+        for count, row in enumerate(self.rows):
+            if (count < 3):
+                box1.append(row[:3])
+                box2.append(row[3:6])
+                box3.append(row[6:])
+            elif (count >= 3 and count < 6):
+                box4.append(row[:3])
+                box5.append(row[3:6])
+                box6.append(row[6:])
+            else:
+                box7.append(row[:3])
+                box8.append(row[3:6])
+                box9.append(row[6:])
+        self.boxes.append(box1)
+        self.boxes.append(box2)
+        self.boxes.append(box3)
+        self.boxes.append(box4)
+        self.boxes.append(box5)
+        self.boxes.append(box6)
+        self.boxes.append(box7)
+        self.boxes.append(box8)
+        self.boxes.append(box9)
+        
+class Box:
+    def __init__(self):
+        # TODO
