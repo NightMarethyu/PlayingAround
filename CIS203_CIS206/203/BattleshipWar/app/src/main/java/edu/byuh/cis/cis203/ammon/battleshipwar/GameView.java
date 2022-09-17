b55222a4abf395505da7647b7cb40b41e230f9fa
Ammon <ammon42@gmail.com>
2022-09-16 22:28:53 -1000

    Created custom sprites added javadoc comments
    I created custom sprites for the game and added Javadoc style comments.

--- CIS203_CIS206/203/BattleshipWar/app/src/main/java/edu/byuh/cis/cis203/ammon/battleshipwar/GameView.java
+++ CIS203_CIS206/203/BattleshipWar/app/src/main/java/edu/byuh/cis/cis203/ammon/battleshipwar/GameView.java
@@ -8,8 +8,14 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 
-import java.util.ArrayList;
-
+/**
+ * GameView extends the Android View class to render the elements of the Battleship War clone made
+ * as part of the BYUH Fall 2022 CIS 203 class.
+ *
+ * @author Ammon Turner
+ * @version %I%, %G%
+ * @since 0.1
+ */
 public class GameView extends View {
 
     private Paint paint;
@@ -22,6 +28,16 @@ public class GameView extends View {
     private Bitmap lit_sub;
     private Bitmap water;
 
+    /**
+     * The Constructor of the GameView class.
+     * This method calls the constructor of the <code>View</code> class, passing the <code>Context
+     * </code> c parameter. Following the call to super, this will initialize the fields, putting
+     * in the images used as sprites.
+     *
+     * @param c The Context the view is running in.
+     * @see     Context
+     * @since   0.1
+     */
     public GameView(Context c) {
         super(c);
         paint = new Paint();
@@ -36,6 +52,14 @@ public class GameView extends View {
         paint.setColor(Color.WHITE);
     }
 
+    /**
+     * This draws the sprites in the provided <code>Canvas</code>, implemented from
+     * <code>android.view.View</code>.
+     *
+     * @param c The <code>Canvas</code> where the background is drawn
+     * @see     Canvas
+     * @since   0.1
+     */
     @Override
     public void onDraw(Canvas c) {
         c.drawPaint(paint);
@@ -55,6 +79,17 @@ public class GameView extends View {
         c.drawBitmap(lit_sub, (screenWidth*2/5), (screenHeight*4/5), paint);
     }
 
+    /**
+     * This draws the water for the game.
+     * drawWater performs the necessary calculations to draw the water in the correct location. It
+     * will then do the drawing in a simple for loop to cover the screen in a straight line.
+     *
+     * @param c         The <code>Canvas</code> where the water will be drawn
+     * @param width     The width of the screen, used for calculating the number of water sprites to draw
+     * @param height    The height of the screen, used to calculate the position of the sprites
+     * @see             Canvas
+     * @since           0.1
+     */
     private void drawWater(Canvas c, int width, int height) {
         var drawHeight = height/2 + water.getHeight();
         var waterWidth = water.getWidth();
