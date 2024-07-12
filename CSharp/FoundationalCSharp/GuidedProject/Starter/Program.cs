948af63c3e163fe081aa374c6a9127af47a23f5d
Ammon <ammondturner@go.byuh.edu>
2024-07-11 16:22:02 -1000

    Completed Guided Project
    I'm pushing this now because I just finished the first guided project that included some downloadable starter code. Since the course thus far is a little too easy, I decided to make my code more interesting using a Dictionary and some internal functions.

---
+++ CSharp/FoundationalCSharp/GuidedProject/Starter/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+// initialize variables - graded assignments 
+int currentAssignments = 5;
+
+Dictionary<string, int[]> studentScores = new Dictionary<string, int[]>();
+
+studentScores["Sophia"] = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+studentScores["Andrew"] = new int[] { 92, 89, 81, 96, 90, 89 };
+studentScores["Emma"] = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+studentScores["Logan"] = new int[] { 90, 95, 87, 88, 96, 96 };
+studentScores["Becky"] = new int[] { 92, 91, 90, 91, 92, 92, 92 };
+studentScores["Chris"] = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
+studentScores["Eric"] = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
+studentScores["Gregor"] = new int[] { 91, 91, 91, 91, 91, 91, 91 };
+
+Console.WriteLine("Student\t\tGrade\n");
+
+foreach (var student in studentScores)
+{
+  decimal average = calculateGrades(student.Value);
+  Console.WriteLine($"{student.Key}\t\t{average.ToString("#.#")}\t{getLetterGrade(average)}");
+}
+
+string getLetterGrade(decimal score)
+{
+  if (score >= 97)
+    return "A+";
+  else if (score >= 93)
+    return "A";
+  else if (score >= 90)
+    return "A-";
+  else if (score >= 87)
+    return "B+";
+  else if (score >= 83)
+    return "B";
+  else if (score >= 80)
+    return "B-";
+  else if (score >= 77)
+    return "C+";
+  else if (score >= 73)
+    return "C";
+  else if (score >= 70)
+    return "C-";
+  else if (score >= 67)
+    return "D+";
+  else if (score >= 63)
+    return "D";
+  else if (score >= 60)
+    return "D-";
+  else
+    return "F";
+}
+
+decimal calculateGrades(int[] scores)
+{
+  int total = 0;
+  int gradedAssignments = 0;
+  foreach (int score in scores)
+  {
+    if (gradedAssignments < currentAssignments)
+    {
+      total += score;
+      gradedAssignments++;
+    }
+    else
+    {
+      total += score / 10;
+    }
+  }
+  return (decimal)total / currentAssignments;
+}
+
+Console.WriteLine("\n\rPress any key to exit.");
+Console.ReadLine();
--- CSharp/FoundationalCSharp/TestProject/Program.cs
+++ CSharp/FoundationalCSharp/TestProject/Program.cs
@@ -1,6 +1,168 @@
-﻿Random dice = new();
+﻿
+// My notes from Unit 4 of "Get started with C#, Part 2/Call methods from the .NET Class Library using C#" on Microsoft Learn
+Console.WriteLine("Module Name: Call methods from the .NET Class Library using C#");
+Random dice = new();
 for (int i = 0; i < 20; i++)
 {
-  int roll = dice.Next(1, 21);
-  Console.WriteLine(roll);
+  Console.Write($"{dice.Next(1, 21)}, ");
 }
+Console.WriteLine(dice.Next(1, 21));
+
+Console.WriteLine($"First role: {dice.Next()}");
+Console.WriteLine($"Second role: {dice.Next(101)}");
+Console.WriteLine($"Third role: {dice.Next(50, 101)}");
+
+int bigNumber = 2147483647;
+Console.WriteLine($"Big number: {bigNumber}");
+Console.WriteLine($"Bigger number: {bigNumber + 1}");
+
+// My notes from Unit 5 of "Get started with C#, Part 2/Call methods from the .NET Class Library using C#" on Microsoft Learn
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+int advantage = Math.Max(dice.Next(1, 21), dice.Next(1, 21));
+
+Console.WriteLine($"Roll with advantage: {advantage}\n");
+
+// Starting The module "Add decision logic to your code using `if`, `else`, and `else if` statements in C#"
+Console.WriteLine("Starting Module: Add decision logic to your code using `if`, `else`, and `else if` statements in C#\n");
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+  if ((roll1 == roll2) && (roll2 == roll3))
+  {
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    total += 6;
+  }
+  else
+  {
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+  }
+}
+
+if (total >= 16)
+{
+  Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+  Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+  Console.WriteLine("You win a trip for two!");
+}
+else
+{
+  Console.WriteLine("You win a kitten!");
+}
+Console.WriteLine();
+
+// `if`, `else`, and `else if` "challenge" assignment
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 0)
+{
+  Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+  discountPercentage = 20;
+  Console.WriteLine($"Your subscription expires within a day!");
+}
+else if (daysUntilExpiration <= 5)
+{
+  discountPercentage = 10;
+  Console.WriteLine($"Your subscription expires within {daysUntilExpiration} days.");
+}
+else if (daysUntilExpiration <= 10)
+{
+  Console.WriteLine($"Your subscription will expire soon. Renew now!");
+}
+
+if (discountPercentage > 0)
+{
+  Console.WriteLine($"Renew now and save {discountPercentage}%.\n");
+}
+
+// Notes from the Arrays module from "Get started with C#, Part 2" on Microsoft Learn
+Console.WriteLine("Starting Module: Arrays\n");
+
+/* string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789"; */
+// fraudulentOrderIDs[3] = "D000"; // This will throw an IndexOutOfRangeException
+
+string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+
+Console.WriteLine($"First fraudulent order ID: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second fraudulent order ID: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third fraudulent order ID: {fraudulentOrderIDs[2]}\n");
+
+fraudulentOrderIDs[0] = "F000";
+Console.WriteLine($"Reassigning the first fraudulent order ID: {fraudulentOrderIDs[0]}\n");
+Console.WriteLine($"The fraudulent order IDs array has {fraudulentOrderIDs.Length} elements.\n");
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+foreach (int items in inventory)
+{
+  sum += items;
+  bin++;
+  Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+Console.WriteLine($"We have {sum} items in inventory.");
+
+string[] orderNumbers = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+Console.WriteLine("Orders that start with 'B':");
+foreach (string order in orderNumbers)
+{
+  if (order.StartsWith("B"))
+  {
+    Console.WriteLine(order);
+  }
+}
+Console.WriteLine("\n");
+
+// Notes from Readable Code module from "Get started with C#, Part 2" on Microsoft Learn
+
+/*
+  This code reverses the message, counts the number of times 
+  the character 'o' appears, and then prints the results
+  to the console window.
+ */
+
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = originalMessage.ToCharArray();
+Array.Reverse(message);
+
+int letterCount = 0;
+
+foreach (char letter in message)
+{
+  if (letter == 'o')
+  {
+    letterCount++;
+  }
+}
+
+string newMessage = new String(message);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
